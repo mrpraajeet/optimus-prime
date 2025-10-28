f97e1396339cd7804b6d9c64549f7a54b403a5a6
mrpraajeet <mrpraajeet@gmail.com>
2025-10-28 22:01:49 +0530

    Fix Test Package

--- src/test/kotlin/optimus/OptimusPrimeApplicationTests.kt
+++ src/test/kotlin/optimus/OptimusPrimeApplicationTests.kt
@@ -1,4 +1,4 @@
-package dev.praajeet.optimus
+package optimus
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class OptimusPrimeApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+  @Test
+  fun contextLoads() {
+  }
 }
